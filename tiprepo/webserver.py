672283df9fa1068e52fba431ca584d4ab91f8d8a
Mustafa <mus@musalbas.com>
2014-05-09 00:31:12 +0100

    Add main database connection

--- tiprepo/webserver.py
+++ tiprepo/webserver.py
@@ -2,6 +2,7 @@ import web
 
 from tiprepo.models.index import Index
 from tiprepo.models.viewrepo import ViewRepo
+from tiprepo.config import MYSQL_HOST, MYSQL_DB, MYSQL_USER, MYSQL_PASS
 
 #web.config.debug = False
 
@@ -11,6 +12,14 @@ urls = (
 )
 
 app = web.application(urls, globals())
+
+db = web.database(dbn='mysql', host=MYSQL_HOST, db=MYSQL_DB,
+                  user=MYSQL_USER, pw=MYSQL_PASS)
+
+def hook():
+    web.ctx.db = db
+
+app.add_processor(web.loadhook(hook))
 
 def run():
     app.run()
